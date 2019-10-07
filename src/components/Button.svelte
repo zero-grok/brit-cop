6a672a64e73e8f624723a0ccdeebe2ebcf94e240
Jeffrey Fate <fatejd@gmail.com>
2019-10-06 20:53:55 -0700

    feat(index): Initial commit

---
+++ src/components/Button.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let buttonText;
+</script>
+
+<style>
+	button {
+    text-align: center;
+    text-transform: uppercase;
+    font-weight: 600;
+		margin: 0 auto;
+		font-size: 2em;
+		margin: 0 0 0.5em 0;
+    padding: 0.3em 1em;
+    width: 100%
+	}
+</style>
+
+<button>{buttonText}</button>
---
+++ src/routes/index.svelte
@@ -0,0 +1,63 @@
+<style>
+	body {
+		background-color: blueviolet;
+	}
+
+	h1, figure, p, button {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	button {
+		font-size: 2em;
+		margin: 0 0 0.5em 0
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+
+</style>
+
+<script>
+	import Button from '../components/Button.svelte'
+</script>
+
+<svelte:head>
+	<title>Looking for a side project?</title>
+</svelte:head>
+
+<h1>Looking for a side project?</h1>
+<Button buttonText={'Yes'} />
+<Button buttonText={"What's a side project?"} />
+<Button buttonText={'Why should I have one?'} />
+
+<!-- <figure>
+	<img alt='Borat' src='great-success.png'>
+	<figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p> -->
